1faf48959773550eba841a25967cdcbdf8e9590a
vtwqajjq <vitywqajjq@icloud.com>
2023-09-05 21:59:20 +0300

    I started by creating a Todolist component, also typed props for it

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,17 +1,28 @@
 import React from "react";
 
-export function TodoList() {
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+type TodolistPropsType = {
+    title: string
+    tasks: TaskType[]
+}
+
+export function TodoList(props: TodolistPropsType) {
     return (
         <div>
-            <h3>What to learn</h3>
+            <h3>{props.title}</h3>
             <div>
                 <input/>
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
             </ul>
             <div>
                 <button>All</button>
